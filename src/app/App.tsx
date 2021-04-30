e3c2960db50b18376bbfc986ee18e7004d2311b1
jaywcjlove <398188662@qq.com>
2021-05-01 03:06:54 +0800

    feat: Add code highlight.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -27,12 +27,12 @@ export default function App() {
           </div>
         </div>
         <div className={styles.code}>
-          <Code lang="SwiftUI" code={`Color(red: ${color.r / 100}, green: ${color.g / 100}, blue: ${color.b / 100}).opacity(${color.a})`} />
-          <Code lang="Swift for iOS" code={`UIColor(red: ${color.r / 100}, green: ${color.g / 100}, blue: ${color.b / 100}, alpha: ${color.a})`} />
-          <Code lang="Swift for macOS" code={`NSColor(red: ${color.r / 100}, green: ${color.g / 100}, blue: ${color.b / 100}, alpha: ${color.a})`} />
-          <Code lang="Objective-C for iOS" code={`[UIColor colorWithRed: ${color.r / 100} green: ${color.g / 100} blue: ${color.b / 100} alpha: ${color.a}];`} />
-          <Code lang="Objective-C for macOS" code={`[NSColor colorWithCalibratedRed: ${color.r / 100} green: ${color.g / 100} blue: ${color.b / 100} alpha: ${color.a}];`} />
-          <Code lang="Xamarin (C#)" code={`new UIColor(red: ${color.r / 100}f, green: ${color.g / 100}f, blue: ${color.b / 100}f, alpha: ${color.a}f)`} />
+          <Code lang="swift" title="SwiftUI" code={`Color(red: ${color.r / 100}, green: ${color.g / 100}, blue: ${color.b / 100}).opacity(${color.a})`} />
+          <Code lang="swift" title="Swift for iOS" code={`UIColor(red: ${color.r / 100}, green: ${color.g / 100}, blue: ${color.b / 100}, alpha: ${color.a})`} />
+          <Code lang="swift" title="Swift for macOS" code={`NSColor(red: ${color.r / 100}, green: ${color.g / 100}, blue: ${color.b / 100}, alpha: ${color.a})`} />
+          <Code lang="objectivec" title="Objective-C for iOS" code={`[UIColor colorWithRed: ${color.r / 100} green: ${color.g / 100} blue: ${color.b / 100} alpha: ${color.a}];`} />
+          <Code lang="objectivec" title="Objective-C for macOS" code={`[NSColor colorWithCalibratedRed: ${color.r / 100} green: ${color.g / 100} blue: ${color.b / 100} alpha: ${color.a}];`} />
+          <Code lang="csharp" title="Xamarin (C#)" code={`new UIColor(red: ${color.r / 100}f, green: ${color.g / 100}f, blue: ${color.b / 100}f, alpha: ${color.a}f)`} />
         </div>
       </div>
     </div>
--- src/app/Code.tsx
+++ src/app/Code.tsx
@@ -1,15 +1,23 @@
 import copyTextToClipboard from '@uiw/copy-to-clipboard';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-csharp';
+import 'prismjs/components/prism-swift';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-objectivec';
 import styles from './Code.module.css';
+import './prism.css';
 
 type CodeProps = {
+  title: string;
   lang: string;
   code: string;
 }
 
 export default function Code(props: CodeProps) {
-  const { lang, code } = props || {}
+  const { title, lang, code } = props || {}
   const [isCopy, setIsCopy] = useState(false)
+  const [html, setHtml] = useState('')
   function copyHandle() {
     setIsCopy(true)
     copyTextToClipboard(code || '', (isCopy) => {
@@ -19,15 +27,26 @@ export default function Code(props: CodeProps) {
       }, 1000)
     });
   }
+  useEffect(() => {
+    let html = ''
+    if (lang == 'swift') {
+      html = Prism.highlight(code, Prism.languages.swift, 'swift');
+    } else if (lang == 'csharp') {
+      html = Prism.highlight(code, Prism.languages.csharp, 'csharp');
+    } else if (lang == 'objectivec') {
+      html = Prism.highlight(code, Prism.languages.objectivec, 'objectivec');
+    }
+    setHtml(html)
+  }, [code])
   return (
     <div className={styles.warpper}>
       <div className={styles.tool}>
-        <div className={styles.title}>{lang}</div>
+        <div className={styles.title}>{title}</div>
         <div className={`${styles.copy} ${isCopy ? styles.copied: ''}`} onClick={copyHandle}>Copy</div>
       </div>
-      <div className={styles.code}>
-        {code}
-      </div>
+      <pre className={styles.code}>
+        <code className={`language-${lang}`} dangerouslySetInnerHTML={{ __html: html}} />
+      </pre>
     </div>
   )
 }
